cfce86aec32d70273331393565e97eb17c180ad5
chanychi <chan.ychi@gmail.com>
2023-06-28 05:32:44 -0500

    Rate limiter

--- server/index.js
+++ server/index.js
@@ -4,20 +4,19 @@ const mongoose = require('mongoose')
 const kpiRoutes = require("./routes/kpi.js");
 const cors = require('cors');
 const helmet = require('helmet');
-// const rateLimit = require('express-rate-limit')
-
+const rateLimit = require('express-rate-limit')
 
 const app = express();
 const PORT = process.env.PORT || 3000
 dotenv.config();
 
-// const limiter = rateLimit({
-//   windowMs: 1000, // 1 second
-//   max: 1, // 1 request per windowMs
-//   message: 'Too many requests, please try again later.',
-// });
+const limiter = rateLimit({
+  windowMs: 1000, // 1 second
+  max: 1, // 1 request per windowMs
+  message: 'Too many requests, please try again later.',
+});
 
-// app.use(limiter)
+app.use(limiter)
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
@@ -25,7 +24,7 @@ app.use(cors());
 // app.disable('x-powered-by')
 
 app.use("/kpi", kpiRoutes);
-// app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,

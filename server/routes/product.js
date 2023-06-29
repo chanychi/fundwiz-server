9b5ab91ff21be74df0d3edb837b673fd3b3db0cc
chanychi <chan.ychi@gmail.com>
2023-06-29 02:18:24 -0500

    Fixed bug with request

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const productRequest = require('../controllers/productController');
+const productRequest = require('../controllers/ProductController.js');
 const router = express.Router();
 const product = new productRequest()
 
